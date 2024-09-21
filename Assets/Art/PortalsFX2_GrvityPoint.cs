9a0bc5fd618bed879b07f1c1386a102f04d9bade
周雨霆 <PC3@versee.tech>
2024-09-21 19:34:48 +0800

    初版

--- Assets/Art/PortalsFX2_GrvityPoint.cs
+++ Assets/Art/PortalsFX2_GrvityPoint.cs
@@ -11,18 +11,26 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
     public float lifeTimeReduce = 0.99f;
     ParticleSystem ps;
     ParticleSystem.Particle[] particles;
-
+    private Vector3 lastTransform;
+    private bool isleft;
     ParticleSystem.MainModule mainModule;
 
     void Start()
     {
         ps = GetComponent<ParticleSystem>();
         mainModule = ps.main;
+        lastTransform = Target.position;
     }
 
     void Update()//后更新是在更新后，保证update里动完了之后再获取的确定坐标
     {
+        
         if (Target == null) return;
+        if (lastTransform!= Target.position)
+        {
+            isleft = Target.position.x < lastTransform.x;
+            lastTransform= Target.position;
+        }
         var maxParticles = mainModule.maxParticles;
         if (particles == null || particles.Length < maxParticles)//创建的particles不满粒子
         {
@@ -38,7 +46,7 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
         for (int i = 0; i < particleCount; i++)
         {
             var distanceToParticle = targetTransformedPosition - particles[i].position;//每个particles到目标的距禓
-            distanceToParticle.y = 0;
+            distanceToParticle.z = 0;
             if (distanceToParticle.magnitude > maxInfluenceDistance)
             {
                 continue;
@@ -46,27 +54,48 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
             if (StopDistance > 0.001f && distanceToParticle.magnitude >= StopDistance)//如果在停止距离范围内且能移
             {
                 particles[i].velocity=Vector3.zero;
-                Vector3 orthogonalTargetTransformedPosition = Vector3.Cross(Vector3.up, -distanceToParticle).normalized;//目标位置的正交位置
-                particles[i].velocity += orthogonalTargetTransformedPosition.normalized *  Time.deltaTime * dragForce;
+                Vector3 orthogonalTargetTransformedPosition = Vector3.Cross(Vector3.back, -distanceToParticle).normalized;//目标位置的正交位置
+                particles[i].velocity += orthogonalTargetTransformedPosition.normalized *  Time.deltaTime * Mathf.Sign((distanceToParticle.x)*dragForce);
                 
             }
             else
             {
-                if (distanceToParticle.z < 0)
+                if (isleft)
                 {
-                    var directionToTarget = Vector3.Normalize(distanceToParticle);//目标方向
-                    var seekForce = directionToTarget* Time.deltaTime * followForce;//方向*单位时间力*力大小的速度
-                    particles[i].velocity += seekForce;//速度上改变
-               
+                    if (distanceToParticle.x > 0)
+                    {
+                        var directionToTarget = Vector3.Normalize(distanceToParticle);//目标方向
+                        var seekForce = directionToTarget* Time.deltaTime * followForce;//方向*单位时间力*力大小的速度
+                        particles[i].velocity += seekForce;//速度上改变
+                        particles[i].velocity -= new Vector3(0.01f,0,0)*Mathf.Sign((distanceToParticle.x));
+
+                    }
+                    else
+                    {
+                        Vector3 orthogonalTargetTransformedPosition = Vector3.Cross(Vector3.back, -distanceToParticle).normalized;//目标位置的正交位置
+                        particles[i].velocity += orthogonalTargetTransformedPosition.normalized *  Time.deltaTime *dragForce;
+                        particles[i].velocity+= new Vector3(0.05f,0,0)*Mathf.Sign((distanceToParticle.x));
+                    }
                 }
                 else
                 {
-                    Vector3 orthogonalTargetTransformedPosition = Vector3.Cross(Vector3.up, -distanceToParticle).normalized;//目标位置的正交位置
-                    particles[i].velocity += orthogonalTargetTransformedPosition.normalized *  Time.deltaTime * Mathf.Sign((distanceToParticle.x)*dragForce);
-                    
+                    if (distanceToParticle.x < 0)
+                    {
+                        var directionToTarget = Vector3.Normalize(distanceToParticle);//目标方向
+                        var seekForce = directionToTarget* Time.deltaTime * followForce;//方向*单位时间力*力大小的速度
+                        particles[i].velocity += seekForce;//速度上改变
+                        particles[i].velocity -= new Vector3(0.01f,0,0)*Mathf.Sign((distanceToParticle.x));
+
+                    }
+                    else
+                    {
+                        Vector3 orthogonalTargetTransformedPosition = Vector3.Cross(Vector3.back, -distanceToParticle).normalized;//目标位置的正交位置
+                        particles[i].velocity += orthogonalTargetTransformedPosition.normalized *  Time.deltaTime *dragForce;
+                        particles[i].velocity+= new Vector3(0.05f,0,0)*Mathf.Sign((distanceToParticle.x));
+                    }
                 }
-               
-                
+
+                   
             }
         }
 
