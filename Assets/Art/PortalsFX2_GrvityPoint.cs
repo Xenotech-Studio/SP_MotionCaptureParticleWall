ac37677c3c155222d94a491c10575b6437b000ad
周雨霆 <PC3@versee.tech>
2024-09-22 18:17:25 +0800

    加入手波动粒子

--- Assets/Art/PortalsFX2_GrvityPoint.cs
+++ Assets/Art/PortalsFX2_GrvityPoint.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class PortalsFX2_GrvityPoint : MonoBehaviour
 {
-    public Transform Target;//是人的圆柱体吗
+    public Transform Target;
+    public List<GameObject> TargetHands;
     public float followForce = 1;
     public float dragForce = 1;
     public float StopDistance = 0;
@@ -12,8 +15,12 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
     ParticleSystem ps;
     ParticleSystem.Particle[] particles;
     private Vector3 lastTransform;
-    private bool isleft;
+    private int isleft=1;
     ParticleSystem.MainModule mainModule;
+    List<ParticleSystem.Particle> enter=new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> inside=new List<ParticleSystem.Particle>();
+    List<ParticleSystem.Particle> exit=new List<ParticleSystem.Particle>();
+    
 
     void Start()
     {
@@ -25,24 +32,20 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
     void Update()//后更新是在更新后，保证update里动完了之后再获取的确定坐标
     {
         
-        if (Target == null) return;
-        if (lastTransform!= Target.position)
+        if (Target == null||lastTransform== Target.position) return;
+        if (Target.position.x > lastTransform.x&&isleft>0)
         {
-            isleft = Target.position.x < lastTransform.x;
-            lastTransform= Target.position;
+            isleft *=-1 ;
         }
+        lastTransform= Target.position;
         var maxParticles = mainModule.maxParticles;
         if (particles == null || particles.Length < maxParticles)//创建的particles不满粒子
         {
             particles = new ParticleSystem.Particle[maxParticles];//创建满粒子？
         }
         int particleCount = ps.GetParticles(particles);//得到目前ps上的粒子数量
-
-        var targetTransformedPosition = Vector3.zero;
-        if (mainModule.simulationSpace == ParticleSystemSimulationSpace.Local)//获取ps的放缩空间
-            targetTransformedPosition = transform.InverseTransformPoint(Target.position);//括号里变量相对于transform的坐标的值！
-        if (mainModule.simulationSpace == ParticleSystemSimulationSpace.World)
-            targetTransformedPosition = Target.position;
+        var targetTransformedPosition = Target.position;
+        
         for (int i = 0; i < particleCount; i++)
         {
             var distanceToParticle = targetTransformedPosition - particles[i].position;//每个particles到目标的距禓
@@ -53,35 +56,69 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
             }
             if (StopDistance > 0.001f && distanceToParticle.magnitude >= StopDistance)//如果在停止距离范围内且能移
             {
-               particles[i].velocity*=0.6f;
-                /*Vector3 orthogonalTargetTransformedPosition = Vector3.Cross(Vector3.back, -distanceToParticle).normalized;//目标位置的正交位置
-                particles[i].velocity += orthogonalTargetTransformedPosition.normalized *  Time.deltaTime * Mathf.Sign((distanceToParticle.x)*dragForce);*/
-                
+                /*particles[i].velocity*=0.2f;*/
             }
             else
             {
-                if (isleft)
-                {
-                    
-                        /*var directionToTarget = Vector3.Normalize(distanceToParticle);//目标方向
-                        var seekForce = directionToTarget* Time.deltaTime * followForce;//方向*单位时间力*力大小的速度
-                        particles[i].velocity += seekForce;//速度上改变*/
-                        particles[i].velocity -= new Vector3( Time.deltaTime * followForce, 0, 0);
-                        particles[i].remainingLifetime *= lifeTimeReduce;
-                       
+                particles[i].velocity -= new Vector3( Time.deltaTime * followForce,0, 0)*isleft;
+                particles[i].remainingLifetime*=lifeTimeReduce;
+                
 
+            }
+            
+        }
+        ps.SetParticles(particles, particleCount);
+    }
 
-                }
-                else
+    private void OnParticleTrigger()
+    {
+        int numEnter = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enter);
+        int numinside = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Inside,inside);
+        int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit,exit);
+        for (int i = 0; i < numEnter; i++)
+        {
+            for (int j = 0; j < TargetHands.Count; j++)
+            {
+                if (TargetHands[j].GetComponent<Collider>().bounds.Contains(enter[i].position))
                 {
-                    particles[i].velocity += new Vector3( Time.deltaTime * followForce, 0, 0);
-                    particles[i].remainingLifetime *= lifeTimeReduce;
+                    ParticleSystem.Particle p = enter[i];
+                    Vector3 targetDirection= enter[i].position - TargetHands[j].transform.position;
+                    targetDirection.z = 0;
+                    Vector3 force=Vector3.Normalize(targetDirection) * Time.deltaTime *
+                                  dragForce*2;
+                    force.z *= 0.1f;
+                    p.velocity += force;
+                    enter[i] = p;
                 }
-        
-                   
+            }
+        }
+        for (int i = 0; i < numinside; i++)
+        {
+            for (int j = 0; j < TargetHands.Count; j++)
+            {
+                if (TargetHands[j].GetComponent<Collider>().bounds.Contains(inside[i].position))
+                {
+                    ParticleSystem.Particle p =inside[i];
+                    Vector3 targetDirection= inside[i].position - TargetHands[j].transform.position;
+                    targetDirection.z = 0;
+                    Vector3 force=Vector3.Normalize(targetDirection) * Time.deltaTime *
+                                  dragForce*2;
+                    force.z *= 0.1f;
+                    p.velocity += force;
+                    inside[i] = p;
+                }
             }
         }
 
-        ps.SetParticles(particles, particleCount);
+        for (int i = 0; i < numExit; i++)
+        {
+            ParticleSystem.Particle p =exit[i];
+            p.velocity *= 0.1f;
+            exit[i] = p;
+        }
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Inside,inside);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter,enter);
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit,exit);
     }
 }
