cfafa74e81441ec0b787894da7ffdd873684ce15
周雨霆 <PC3@versee.tech>
2024-10-04 21:25:29 +0800

    新单人版

--- Assets/Art/PortalsFX2_GrvityPoint.cs
+++ Assets/Art/PortalsFX2_GrvityPoint.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,6 +11,7 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
     public List<GameObject> TargetHands;
     public float followForce = 1;
     public float dragForce = 1;
+    public float nearDistance = 0;
     public float StopDistance = 0;
     public float handDistance = 0;
     public float maxInfluenceDistance = 0;
@@ -42,11 +44,11 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
         int particleCount = ps.GetParticles(particles);
         var targetTransformedPosition = Target.position;
 
-        if (Mathf.Abs(Target.position.x - lastTransform.x)<0.2f)
+        if (Mathf.Abs(Target.position.x - lastTransform.x)<1f)
         {
             for (int i = 0; i < particleCount; i++)
             {
-                for (int j = 0; j < TargetHands.Count; j++)
+                /*for (int j = 0; j < TargetHands.Count; j++)
                 {
                     Vector3 targetDirection = particles[i].position - TargetHands[j].transform.position;
                     targetDirection.z = 0;
@@ -77,8 +79,8 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
                     distanceToParticle.magnitude < 2 * maxInfluenceDistance) //如果在停止距离范围内且能移
                 {
                     /*particles[i].velocity += new Vector3(Time.deltaTime * followForce,
-                                                 0, 0) * Mathf.Sign(-distanceToParticle.x);*/
-                }
+                                                 0, 0) * Mathf.Sign(-distanceToParticle.x);#1#
+                }*/
             }
             ps.SetParticles(particles, particleCount);
             lastTransform = Target.position;
@@ -103,7 +105,7 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
             var distanceToParticle = targetTransformedPosition - particles[i].position; //每个particles到目标的距禓
             for (int j = 0; j < TargetHands.Count; j++)
             {
-                Vector3 targetDirection = particles[i].position - TargetHands[j].transform.position;
+                /*Vector3 targetDirection = particles[i].position - TargetHands[j].transform.position;
                 targetDirection.z = 0;
                 if (targetDirection.magnitude >= StopDistance &&
                     targetDirection.magnitude < maxInfluenceDistance &&
@@ -123,7 +125,7 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
                     particles[i].rotation3D += new Vector3(0,0,10f*Time.deltaTime);
                     particles[i].angularVelocity3D += new Vector3(0,300f*Time.deltaTime,300f*Time.deltaTime);
                     particles[i].remainingLifetime *= lifeTimeReduce;
-                }
+                }*/
             }
             if (distanceToParticle.magnitude > maxInfluenceDistance)
             {
@@ -132,24 +134,30 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
 
             if (distanceToParticle.magnitude >= StopDistance) //如果在停止距离范围内且能移
             {
-                particles[i].velocity *= 0.95f;
+                particles[i].velocity *= 0.9f;
                 /*particles[i].position += new Vector3(0,0 ,100f) * isleft /
                                          distanceToParticle.magnitude;*/
             }
-            else
+           
+            else if(distanceToParticle.magnitude<=StopDistance)
+            { 
+                 //particles[i].rotation += 270f*Time.deltaTime;//原地打转，稍微带点波动
+                 particles[i].rotation3D += new Vector3(0,60*Time.deltaTime,0);//朝横、竖、原地转
+                 particles[i].position += new Vector3(-isleft, Mathf.Sign(distanceToParticle.y)/10,0 )*Time.deltaTime*followForce;//z轴没用，x平点也行，y轴先随便用自带的吸引,y吸了，力还挺大
+                 particles[i].velocity += new Vector3(-isleft, Mathf.Sign(distanceToParticle.y),0 )*Time.deltaTime*followForce*5;
+                 particles[i].remainingLifetime *= lifeTimeReduce;
+                // particles[i].velocity -= new Vector3((Time.deltaTime * followForce*2f+Random.Range(2f,3f) )* isleft, Time.deltaTime * distanceToParticle.y*4*Random.Range(-5f,2f), 0)*0.1f;
+                
+              /* particles[i].startSize *= Random.Range(1.01f,1.2f);
+               
+                
+                
+               */
+              accumulatedTime += Time.deltaTime;
+            }
+            if(distanceToParticle.magnitude<=nearDistance&&Mathf.Sign(distanceToParticle.x)==Mathf.Sign(isleft))
             {
-
-                 particles[i].position -= new Vector3((Time.deltaTime * followForce + Random.Range(2f, 3f)) * isleft,
-                                              Time.deltaTime * distanceToParticle.y * +Random.Range(-10f, 10f),
-                                              0) /
-                                         distanceToParticle.magnitude;
-                particles[i].velocity -= new Vector3((Time.deltaTime * followForce*2f+Random.Range(2f,3f) )* isleft, Time.deltaTime * distanceToParticle.y*4*Random.Range(-5f,2f), 0)*0.1f;
-                //particles[i].rotation += 10f*Time.deltaTime;
-               particles[i].startSize *= Random.Range(1.01f,1.2f);
-              /* particles[i].rotation3D += new Vector3(0,0,10f*Time.deltaTime);*/
-                particles[i].angularVelocity3D += new Vector3(0,20f*Time.deltaTime,20f*Time.deltaTime);
-                particles[i].remainingLifetime *= lifeTimeReduce;
-
+                //particles[i].velocity *= -1f;
             }
         }
         ps.SetParticles(particles, particleCount);
@@ -162,49 +170,66 @@ public class PortalsFX2_GrvityPoint : MonoBehaviour
             int numExit = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Exit,exit);
             for (int i = 0; i < numEnter; i++)
             {
-                for (int j = 0; j < TargetHands.Count; j++)
-                {
-                    if (TargetHands[j].GetComponent<Collider>().bounds.Contains(enter[i].position))
-                    {
-                        ParticleSystem.Particle p = enter[i];
-                        Vector3 targetDirection= enter[i].position - TargetHands[j].transform.position;
-                        targetDirection.z = 0;
-                        Vector3 force=Vector3.Normalize(targetDirection) * Time.deltaTime *
-                                      dragForce/targetDirection.magnitude;
-                        force.z *= 0f;
-                        p.velocity += force;
-                        enter[i] = p;
-                    }
-                }
-            }
-            for (int i = 0; i < numinside; i++)
-            {
-                for (int j = 0; j < TargetHands.Count; j++)
-                {
-                    if (TargetHands[j].GetComponent<Collider>().bounds.Contains(inside[i].position))
-                    {
-                        ParticleSystem.Particle p =inside[i];
-                        Vector3 targetDirection= inside[i].position - TargetHands[j].transform.position;
-                        targetDirection.z = 0;
-                        Vector3 force=Vector3.Normalize(targetDirection) * Time.deltaTime *
-                                      dragForce/targetDirection.magnitude;
-                        force.z *= 0f;
-                        p.velocity += force;
-                        inside[i] = p;
-                    }
-                }
-            }
-    
-            for (int i = 0; i < numExit; i++)
-            {
-                ParticleSystem.Particle p =exit[i];
-                p.velocity *= 0f;
-                exit[i] = p;
-            }
-    
-            ps.SetTriggerParticles(ParticleSystemTriggerEventType.Inside,inside);
-            ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter,enter);
-            ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit,exit);
-        }*/
+            
+            
+            
+            particles[i].position -= new Vector3((followForce + Random.Range(2f, 3f)) * isleft*Time.deltaTime ,
+                                              Time.deltaTime * distanceToParticle.y * +Mathf.Sin(5*accumulatedTime)*1000,
+                                              0)/distanceToParticle.magnitude ;
+                particles[i].velocity -= new Vector3((Time.deltaTime * followForce*2f+Random.Range(2f,3f) )* isleft, Time.deltaTime * distanceToParticle.y*4*Random.Range(-5f,2f), 0)*0.1f;
+                particles[i].rotation += 10f*Time.deltaTime;
+              /* particles[i].startSize *= Random.Range(1.01f,1.2f);
+               particles[i].rotation3D += new Vector3(0,0,10f*Time.deltaTime);
+                particles[i].angularVelocity3D += new Vector3(0,20f*Time.deltaTime,20f*Time.deltaTime);
+                particles[i].remainingLifetime *= lifeTimeReduce;
+                accumulatedTime += Time.deltaTime;*/
+            
+            
+            
+            
+        /*  for (int j = 0; j < TargetHands.Count; j++)
+         {
+             if (TargetHands[j].GetComponent<Collider>().bounds.Contains(enter[i].position))
+             {
+                 ParticleSystem.Particle p = enter[i];
+                 Vector3 targetDirection= enter[i].position - TargetHands[j].transform.position;
+                 targetDirection.z = 0;
+                 Vector3 force=Vector3.Normalize(targetDirection) * Time.deltaTime *
+                               dragForce/targetDirection.magnitude;
+                 force.z *= 0f;
+                 p.velocity += force;
+                 enter[i] = p;
+             }
+         }
+     }
+     for (int i = 0; i < numinside; i++)
+     {
+         for (int j = 0; j < TargetHands.Count; j++)
+         {
+             if (TargetHands[j].GetComponent<Collider>().bounds.Contains(inside[i].position))
+             {
+                 ParticleSystem.Particle p =inside[i];
+                 Vector3 targetDirection= inside[i].position - TargetHands[j].transform.position;
+                 targetDirection.z = 0;
+                 Vector3 force=Vector3.Normalize(targetDirection) * Time.deltaTime *
+                               dragForce/targetDirection.magnitude;
+                 force.z *= 0f;
+                 p.velocity += force;
+                 inside[i] = p;
+             }
+         }
+     }
+
+     for (int i = 0; i < numExit; i++)
+     {
+         ParticleSystem.Particle p =exit[i];
+         p.velocity *= 0f;
+         exit[i] = p;
+     }
+
+     ps.SetTriggerParticles(ParticleSystemTriggerEventType.Inside,inside);
+     ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter,enter);
+     ps.SetTriggerParticles(ParticleSystemTriggerEventType.Exit,exit);
+ }*/
     
 }
---
+++ Assets/controller.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Crest;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject vectorField1;
+    public GameObject vectorField2;
+    public GameObject vectorField3;
+    public GameObject vectorField4;
+    public ShapeFFT shapeFFT;
+    public OceanRenderer oceanRenderer;
+    public Vector3 lastPlayerPos;
+    private float stillTime;
+    public int isleft = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPlayerPos=player.transform.position;
+        shapeFFT._windSpeed= 0;
+        transform.position.Set(player.transform.position.x,transform.position.y,transform.position.z);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Mathf.Abs(player.transform.position.x -  lastPlayerPos.x)<0.1f)//记得换成误差型
+        {
+            vectorField1.SetActive(false);
+            vectorField2.SetActive(false);
+            vectorField3.SetActive(false);
+            vectorField4.SetActive(false);
+            return;
+        }
+        else if (player.transform.position.x > lastPlayerPos.x && isleft > 0 && player.transform.position.x - lastPlayerPos.x > 0.1f)
+        {
+            isleft *= -1;
+        }
+
+        if (player.transform.position.x < lastPlayerPos.x && isleft < 0 &&  lastPlayerPos.x -player.transform.position.x> 0.1f)
+        {
+            isleft *= -1;
+        }
+        
+        transform.position +=new Vector3(player.transform.position.x-lastPlayerPos.x,0,0);
+       
+        if(isleft>0)
+        { 
+            vectorField1.SetActive(true);
+            vectorField2.SetActive(true);
+        }
+        else
+        {
+            vectorField3.SetActive(true);
+            vectorField4.SetActive(true);
+        }
+        
+        lastPlayerPos=player.transform.position;
+        
+    }
+}
