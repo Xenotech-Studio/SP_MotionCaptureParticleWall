e8327751777a37e167693780d5284cfac38b7892
周雨霆 <PC3@versee.tech>
2024-11-08 21:52:15 +0800

    加入手部运动

--- Assets/relativeTransform.cs
+++ Assets/relativeTransform.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Crest;
 using UnityEngine;
 
 public class relativeTransform : MonoBehaviour
@@ -15,7 +16,11 @@ public class relativeTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (target.transform.position.x - lastPlayerPos.x>0.1f&&gameObject.GetComponent<SphereWaterInteraction>()!=null)
+        {
+            gameObject.GetComponent<SphereWaterInteraction>().enabled = true;
+        }
+  
         transform.position +=new Vector3((target.transform.position.x-lastPlayerPos.x)/20,0,target.transform.position.z-lastPlayerPos.z);
         lastPlayerPos=target.transform.position;
     }
